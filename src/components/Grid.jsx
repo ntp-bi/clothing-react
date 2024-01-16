793bb1f3acc0afcd596ecf384b0f542a2e24d115
ntp-bi <bintp22122002@gmail.com>
2024-01-16 21:11:29 +0700

    grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Grid = props => {
+
+    const style = {
+        gap: props.gap ? `${props.gap}px` : '0'
+    }
+
+    const col = props.col ? `grid-col-${props.col}` : '0'
+    const mdCol = props.col ? `grid-col-${props.mdCol}` : '0'
+    const smCol = props.col ? `grid-col-${props.smCol}` : '0'
+
+    return (
+        <div className={`grid ${col} ${mdCol} ${smCol}`} style={style}>
+            {props.children}
+        </div>
+    )
+}
+
+Grid.propTypes = {
+    col: PropTypes.number.isRequired,
+    mdCol: PropTypes.number,
+    smCol: PropTypes.number,
+    gap: PropTypes.number
+}
+
+export default Grid
