cde242f60b64ed653c6823ba637f218c8d21b1c6
ntp-bi <bintp22122002@gmail.com>
2024-01-17 21:52:38 +0700

    Policy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Helmet from '../components/Helmet'
 import HeroSlider from '../components/HeroSlider'
+import Section, { SectionBody, SectionTitle } from '../components/Section'
+import PolicyCard from '../components/PolicyCard'
+import policy from '../assets/fake-data/policy'
+import Grid from '../components/Grid'
 
 import heroSliderData from '../assets/fake-data/hero-slider'
 
@@ -9,13 +14,38 @@ const Home = () => {
   return (
     <Helmet title='Trang chủ'>
       {/* hero slider */}
-      <HeroSlider 
+      <HeroSlider
         data={heroSliderData}
         control={true}
         auto={true}
         timeOut={5000}
       />
       {/* end hero slider */}
+      {/*policy section*/}
+      <Section>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              policy.map((item, index) =>
+                <Link to='/policy'>
+                  <PolicyCard
+                    key={index}
+                    name={item.name}
+                    description={item.description}
+                    icon={item.icon}
+                  />
+                </Link>
+              )
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/*end policy section*/}
     </Helmet>
   )
 }
