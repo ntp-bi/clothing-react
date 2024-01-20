0752e4683f3b8cd39f45487b8ee697b47a4a82fb
ntp-bi <bintp22122002@gmail.com>
2024-01-20 16:47:47 +0700

    make catagory and checkbox

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,89 @@
 import React from 'react'
 
 import Helmet from '../components/Helmet'
+import Grid from '../components/Grid'
+import ProductCard from '../components/ProductCard'
+import CheckBox from '../components/CheckBox'
+
+import productData from '../assets/fake-data/products'
+import category from '../assets/fake-data/category'
+import colors from '../assets/fake-data/product-color'
+import sizes from '../assets/fake-data/product-size'
 
 const Catalog = () => {
   return (
     <Helmet title='Sản phẩm'>
-      Catalog
+      <div className="catalog">
+        <div className="catalog__filter">
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter__widget__title">
+              danh mục sản phẩm
+            </div>
+            <div className="catalog__filter__widget__content">
+              {
+                category.map((item, index) => (
+                  <div key={index} className='catalog__filter__widget__content__item'>
+                    <CheckBox label={item.display} />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* Màu sắc */}
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter__widget__title">
+              màu sắc
+            </div>
+            <div className="catalog__filter__widget__content">
+              {
+                colors.map((item, index) => (
+                  <div key={index} className='catalog__filter__widget__content__item'>
+                    <CheckBox label={item.display} />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+
+          {/* Kích thước */}
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter__widget__title">
+              màu sắc
+            </div>
+            <div className="catalog__filter__widget__content">
+              {
+                sizes.map((item, index) => (
+                  <div key={index} className='catalog__filter__widget__content__item'>
+                    <CheckBox label={item.display} />
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+        <div className="catalog__content">
+          <Grid
+            col={3}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              productData.getAllProducts().map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+          </Grid>
+        </div>
+      </div>
     </Helmet>
   )
 }
