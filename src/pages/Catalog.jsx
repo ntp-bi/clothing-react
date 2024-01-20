32dd2a1030b25088271cc65d447cdcde448f3f60
ntp-bi <bintp22122002@gmail.com>
2024-01-20 20:12:20 +0700

    make filter category

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 import Helmet from '../components/Helmet'
 import Grid from '../components/Grid'
 import ProductCard from '../components/ProductCard'
 import CheckBox from '../components/CheckBox'
+import Button from '../components/Button'
 
 import productData from '../assets/fake-data/products'
 import category from '../assets/fake-data/category'
@@ -11,6 +12,84 @@ import colors from '../assets/fake-data/product-color'
 import sizes from '../assets/fake-data/product-size'
 
 const Catalog = () => {
+
+  const initFilter = {
+    category: [],
+    color: [],
+    size: []
+  }
+
+  const productList = productData.getAllProducts();
+
+  const [products, setProducts] = useState(productList);
+
+  const [filter, setFilter] = useState(initFilter);
+
+  const filterSelect = (type, checked, item) => {
+    if (checked) {
+      switch (type) {
+        case "CATEGORY":
+          setFilter({ ...filter, category: [...filter.category, item.categorySlug] })
+          break;
+        case "COLOR":
+          setFilter({ ...filter, color: [...filter.color, item.color] })
+          break;
+        case "SIZE":
+          setFilter({ ...filter, size: [...filter.size, item.size] })
+          break;
+        default:
+      }
+    } else {
+      switch (type) {
+        case "CATEGORY":
+          const newCategory = filter.category.filter(e => e !== item.categorySlug)
+          setFilter({ ...filter, category: newCategory })
+          break;
+        case "COLOR":
+          const newColor = filter.color.filter(e => e !== item.color)
+          setFilter({ ...filter, color: newColor })
+          break;
+        case "SIZE":
+          const newSize = filter.size.filter(e => e !== item.size)
+          setFilter({ ...filter, size: newSize })
+          break;
+        default:
+      }
+    }
+  }
+
+  const clearFilter = () => setFilter(initFilter)
+  
+
+  const updateProducts = useCallback(
+    () => {
+      let temp = productList
+
+      if (filter.category.length > 0) {
+        temp = temp.filter(e => filter.category.includes(e.categorySlug))
+      }
+
+      if (filter.color.length > 0) {
+        temp = temp.filter(e => {
+          const check = e.colors.find(color => filter.color.includes(color))
+          return check !== undefined
+        })
+      }
+
+      if (filter.size.length > 0) {
+        temp = temp.filter(e => {
+          const check = e.size.find(size => filter.size.includes(size))
+          return check !== undefined
+        })
+      }
+
+      setProducts(temp)
+    }, [filter, productList])
+
+  useEffect(() => {
+    updateProducts()
+  }, [updateProducts])
+
   return (
     <Helmet title='Sản phẩm'>
       <div className="catalog">
@@ -23,7 +102,11 @@ const Catalog = () => {
               {
                 category.map((item, index) => (
                   <div key={index} className='catalog__filter__widget__content__item'>
-                    <CheckBox label={item.display} />
+                    <CheckBox
+                      label={item.display}
+                      onChange={(input) => filterSelect("CATEGORY", input.checked, item)}
+                      checked={filter.category.includes(item.categorySlug)}
+                    />
                   </div>
                 ))
               }
@@ -39,7 +122,11 @@ const Catalog = () => {
               {
                 colors.map((item, index) => (
                   <div key={index} className='catalog__filter__widget__content__item'>
-                    <CheckBox label={item.display} />
+                    <CheckBox
+                      label={item.display}
+                      onChange={(input) => filterSelect("COLOR", input.checked, item)}
+                      checked={filter.color.includes(item.color)}
+                    />
                   </div>
                 ))
               }
@@ -55,10 +142,25 @@ const Catalog = () => {
               {
                 sizes.map((item, index) => (
                   <div key={index} className='catalog__filter__widget__content__item'>
-                    <CheckBox label={item.display} />
+                    <CheckBox
+                      label={item.display}
+                      onChange={(input) => filterSelect("SIZE", input.checked, item)}
+                      checked={filter.size.includes(item.size)}
+                    />
                   </div>
                 ))
               }
+            </div>
+          </div>
+
+          <div className="catalog__filter__widget">
+            <div className="catalog__filter__widget__content">
+              <Button
+                size="sm"
+                onClick={clearFilter}
+              >
+                xóa bộ lọc
+              </Button>
             </div>
           </div>
         </div>
@@ -70,7 +172,7 @@ const Catalog = () => {
             gap={20}
           >
             {
-              productData.getAllProducts().map((item, index) => (
+              products.map((item, index) => (
                 <ProductCard
                   key={index}
                   img01={item.image01}
